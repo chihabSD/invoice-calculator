c39b7101cc503d82eba2b7690aa0148bf6efeef7
chihab <adamchihab2012@hotmail.com>
2020-03-09 23:50:45 +0000

    prinint

--- src/App.js
+++ src/App.js
@@ -17,10 +17,17 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
+import PDF from "./PDF";
+import Print from "./Print";
+
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 const useStyles = makeStyles(theme => ({
+  main: {
+    display: "flex",
+    flexDirection: "row"
+  },
   date: {
     flexDirection: "row",
     display: "flex",
@@ -32,21 +39,23 @@ const useStyles = makeStyles(theme => ({
     paddingRight: 100
   },
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(4),
     display: "flex",
     flexDirection: "column",
-    alignItems: "center"
+    alignItems: "center",
+    margin: 10,
+    width: "40%"
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: "70%", // Fix IE 11 issue.
     marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2)
+    margin: theme.spacing(2, 0, 1)
   }
 }));
 const App = () => {
@@ -55,16 +64,18 @@ const App = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [period, setPeriod] = useState(0);
+  const [address, setAddress] = useState("")
   const [daysInput, setDaysInput] = useState(0);
   const [cardNumbers, setCardNumbers] = useState(0);
   const [cardCharges, setCardCharges] = useState(0);
   const [beforeTax, setBeforeTax] = useState(0);
   const [total, setTotal] = useState(0);
-  const [totalValue, setTotalValue] = useState(0);
 
+  const [totalValue, setTotalValue] = useState(0);
+  const [restaurant, setRestaurant] = useState("");
 
   // Show Invoice
-  const [showInvoice, setShowInvoice] = useState(false)
+  const [showInvoice, setShowInvoice] = useState(false);
   const handleStart = date => {
     setStartDate(date);
   };
@@ -74,7 +85,7 @@ const App = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const oneDay = 24 // hours*minutes*seconds*milliseconds
     const firstDate = startDate;
     const secondDate = endDate;
     const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
@@ -89,16 +100,20 @@ const App = () => {
     var percent = (calPercentage / 100) * beforetx;
     var afterPercent = values + percent;
     var rounded = Math.round(afterPercent * 100) / 100;
-    var finalValue = total - rounded
-    setTotalValue(finalValue)
-
+    var finalValue = total - rounded;
+    setTotalValue(finalValue);
   };
 
   // Display invoice
-
+  const displayInvoice = e => {
+    setShowInvoice(true);
+    e.preventDefault();
+  };
+  const hidePdf = () => {
+    setShowInvoice(false);
+  };
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
+    <Container component="main" maxWidth="lg" className={classes.main}>
       <div className={classes.paper}>
         <Typography component="h1" variant="h5">
           Invoice Calculator
@@ -139,15 +154,32 @@ const App = () => {
             required
             fullWidth
             onChange={e => setTotal(e.target.value)}
-            
-            
             name="cards"
             type="number"
             label=" Total Value "
-            
             autoFocus
-            />
-           
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            onChange={e => setRestaurant(e.target.value)}
+            name="cards"
+            type="text"
+            label=" Restaurant Name "
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            onChange={e => setAddress(e.target.value)}
+            name="cards"
+            type="text"
+            label=" Restaurant Adress"
+            autoFocus
+          />
+
           <Button
             type="submit"
             fullWidth
@@ -158,25 +190,29 @@ const App = () => {
           >
             Calculate
           </Button>
-        </form>
-        <Button
+          <Button
             type="submit"
             fullWidth
             variant="contained"
             color="secondary"
             onClick={displayInvoice}
-            
           >
             Show invoice
           </Button>
-        <h1>number of days {period} </h1>
+        </form>
+<h2>Start date = {address} </h2>
+<div>
+        <button onClick={() => window.print()}>PRINT</button>
+        <p>Click above button opens print preview with these words on page</p>
+      </div>
+        {/* <h1>number of days {period} </h1>
         <h1>number of days entered {daysInput} </h1>
         <h1>
           Card payment & Admin Fee ({cardNumbers} card orders * 25 ) |{" "}
           {cardCharges}{" "}
         </h1>
         <h3>Before tax {beforeTax} </h3>
-        <h3>Total {total} </h3>
+        <h3>Total {total} </h3> */}
 
         {/* <h3>Food in service charge (period {values.monthly}):</h3>
   <h4>Card payment & admin fees ( {values.cards}  cards * 25c) =  {cardTotal}</h4>
@@ -184,147 +220,10 @@ const App = () => {
   <h3>Sale: {values.totalSale}</h3>
   <h3>Before vat: {beforeVar}</h3> */}
       </div>
+      {showInvoice ? (
+        <Print restaurant={restaurant} startDate={startDate} endDate={endDate} />
+      ) : null}
     </Container>
   );
 };
 export default App;
-
-// export default function App() {
-
-//   // const [values, setValues] = useState({
-//   //   day: "",
-//   //   monthly: '',
-//   //   cards: '',
-//   //   startDate : new Date(),
-//   //   endDate : new Date(),
-//   //   days: 0,
-//   //   totalSale: ''
-//   // })
-//   const [total, setTotal] = useState(44)
-//   const [monthlyValue, setMonthlyValue] = useState("")
-//   const [beforeVar, setBeforeVat] = useState("")
-//   const [cardTotal, setCardTotal] = useState("")
-//   const [subTotal, setSubTotal] = useState("")
-//   const [startDate, setStartDate] = useState(new Date())
-
-//   // const [startDate, setStartDate] = useState(new Date())
-//   // const [endDate, setEndDate] = useState(new Date())
-
-//   // const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-// //   const oneDay = 1000 * 60 * 60 * 24; // hours*minutes*seconds*milliseconds
-// // const firstDate = values.startDate;
-// // const secondDate = values.endDate
-
-// // const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
-
-// // console.log(diffDays)
-
-//  const  handleChange = date => {
-//     setStartDate({
-//       startDate: date
-//     });
-//   };
-
-//   // const updateField = e => {
-//   //   setValues({
-//   //     ...values,
-//   //     [e.target.name]: e.target.value
-//   //   });
-//   // };
-//   // const handleSubmit= () => {
-//   //   setDay(day)
-//   // }
-//   const classes = useStyles();
-
-//   return (
-//     <Container component="main" maxWidth="xs">
-//       <CssBaseline />
-//       <div className={classes.paper}>
-//         <Typography component="h1" variant="h5">
-//           Invoice Calculator
-//         </Typography>
-//         <form className={classes.form} noValidate>
-//           {/* <TextField
-//             margin="normal"
-//             required
-//             fullWidth
-//             onChange={updateField}
-//             value={values.day}
-//             id="day"
-
-//             type="number"
-//             label="Charge per week"
-//             name="day"
-
-//             autoFocus
-//           />
-
-//           <TextField
-//             margin="normal"
-//             required
-//             fullWidth
-//             onChange={updateField}
-//             value={values.monthly}
-
-//             name="monthly"
-//             type="number"
-//             label="Enter days of the month (30, 29)"
-
-//             autoFocus
-//           />
-//           <TextField
-//             margin="normal"
-//             required
-//             fullWidth
-//             onChange={updateField}
-//             value={values.cards}
-
-//             name="cards"
-//             type="number"
-//             label="How many Cards "
-
-//             autoFocus
-//           />
-//           <TextField
-//             margin="normal"
-//             required
-//             fullWidth
-//             onChange={updateField}
-//             value={values.totalSale}
-
-//             name="totalSale"
-//             type="number"
-//             label="Total Sale "
-
-//             autoFocus
-//           /> */}
-
-//           {/* <h2> Total Sale : {monthlyValue}</h2> */}
-//           <Button
-//             type="submit"
-//             fullWidth
-//             variant="contained"
-//             color="primary"
-//             onClick={handleSubmit}
-//             className={classes.submit}
-//           >
-//            Calculate
-//           </Button>
-//         </form>
-//          <h2> Start Date </h2>
-//          <DatePicker
-//         selected={startDate}
-//         onChange={handleChange}
-//       />
-
-//   {/* <h3>Food in service charge (period {values.monthly}):</h3>
-//   <h4>Card payment & admin fees ( {values.cards}  cards * 25c) =  {cardTotal}</h4>
-//   <h3>Subtotal: {subTotal}</h3>
-//   <h3>Sale: {values.totalSale}</h3>
-//   <h3>Before vat: {beforeVar}</h3> */}
-
-//       </div>
-
-//     </Container>
-//   );
-// }
---
+++ src/Print.js
@@ -0,0 +1,165 @@
+import React, { useState } from "react";
+import "./Modal.scss";
+import PrintComponents from "react-print-components";
+import * as moment from 'moment';
+import Pdf from "react-to-pdf";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import ReactToPrint from "react-to-print";
+
+import logo from "./logo.png";
+const useStyles = makeStyles(theme => ({
+  main: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  date: {
+    flexDirection: "row",
+    display: "flex",
+    padding: 5
+  },
+  dateTwo: {
+    display: "flex",
+    flexDirection: "column",
+    paddingRight: 100
+  },
+  paper: {
+    marginTop: theme.spacing(4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    margin: 10,
+    width: "70%"
+  },
+
+  form: {
+    width: "70%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor:'blue'
+  },
+  submit: {
+    margin: theme.spacing(2, 0, 1)
+  }
+}));
+
+const Print = ({ restaurant, startDate,endDate }) => {
+  const classes = useStyles();
+  const ref = React.createRef();
+
+  return (
+    <div className={classes.paper}>
+      <Typography component="h1" variant="h5">
+        your invoice
+      </Typography>
+
+      <PrintComponents
+  trigger={<button>Print</button>}
+>
+ 
+      <form className={classes.form} noValidate >
+        <div className="img">
+          <img src={logo} className="image" />
+        </div>
+        <div className="summary">
+          <h3>Resturant : {restaurant}</h3>
+
+          <div className="summaryTitle">
+            <h1>Invoice Summary </h1>
+            <h3>Period: {moment(new Date(startDate)).format("MMMM Do YYYY")} - {moment(new Date(endDate)).format("MMMM Do YYYY")} </h3>
+          </div>
+          <hr></hr>
+          <div className="tableContainer">
+            <table class="customTable">
+              <thead></thead>
+              <tbody>
+                <tr>
+                  <td>Row 1, Cell 1</td>
+                  <td><p>* This amount will be paid by</p></td>
+                  <td>Row 1, Cell 3</td>
+                </tr>
+                <tr>
+                  <td>Row 2, Cell 1</td>
+                  <td>Row 2, Cell 2</td>
+                  <td>Row 2, Cell 3</td>
+                </tr>
+                <tr>
+                  <td>Row 3, Cell 1</td>
+                  <td>Row 3, Cell 2</td>
+                  <td>Row 3, Cell 3</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="summaryTitle">
+            <h1>Your Invoice</h1>
+          </div>
+          <h3>Invoice Date: 01/Mar/2020</h3>
+          <hr className="style1"></hr>
+        </div>
+      </form>
+</PrintComponents>
+
+      {/* <ReactToPrint
+          trigger={() =>  
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="danger"
+            // onClick={toPdf}
+          >
+            Download the invoice
+          </Button>}
+          content={() => componentRef}
+        /> */}
+
+      {/* <Pdf targetRef={ref} filename="invoice">
+        {({ toPdf }) => (
+         
+        )}
+      </Pdf> */}
+    </div>
+    // <div className="outerContainer">
+    //   <div className="container">
+    //     <div className="btn">
+    //       <Pdf targetRef={ref} filename="invoice">
+    //         {({ toPdf }) => (
+    //           <button className="pdf" onClick={toPdf}>
+    //             Generate Pdf
+    //           </button>
+    //         )}
+    //       </Pdf>
+    //     </div>
+    //     <div ref={ref} className="pdfContainer">
+    //       <div className="imageDiv">
+    //
+    //       </div>
+    //       <div className="restaurantTitle">
+
+    //       <h3> restaurant: {restaurant}</h3>
+    //       <div className="summary">
+    //           <h1>Invoice Summary</h1>
+    //           <h3>Period: 01 Feb 2020 - 29 Feb 2020</h3>
+    //       </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+  );
+};
+
+export default Print;
